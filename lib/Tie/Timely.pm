8552d6722decba97e08f6c3cb00874159017a924
brian d foy <brian.d.foy@gmail.com>
2022-01-20 20:54:59 -0500

    bump copyright year

--- lib/Tie/Timely.pm
+++ lib/Tie/Timely.pm
@@ -59,7 +59,7 @@ brian d foy, C<< <brian.d.foy@gmail.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2005-2021, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2005-2022, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 

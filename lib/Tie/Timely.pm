299fc6b066ce39e9064a42877583e9cc3e3184fa
brian d foy <briandfoy@pobox.com>
2024-02-05 23:51:31 -0500

    Update email address

--- lib/Tie/Timely.pm
+++ lib/Tie/Timely.pm
@@ -59,7 +59,7 @@ brian d foy, C<< <brian.d.foy@gmail.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2005-2023, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2005-2023, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 

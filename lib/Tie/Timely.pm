b924ea6944e16c1f960534c041076ebf5608c0c7
brian d foy <briandfoy@pobox.com>
2026-01-07 10:22:16 -0500

    Updated copyright year

--- lib/Tie/Timely.pm
+++ lib/Tie/Timely.pm
@@ -59,7 +59,7 @@ brian d foy, C<< <brian.d.foy@gmail.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2005-2025, brian d foy <briandfoy@pobox.com>. All rights reserved.
+Copyright © 2005-2026, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
